51423078703bd35792f60e0a0dc475f05e649d39
Alfyando <alfyando2001@gmail.com>
2021-09-03 05:26:01 -0700

    Kriteria 4 : API dapat mengubah data lagu

--- src/api/songs/handler.js
+++ src/api/songs/handler.js
@@ -117,6 +117,38 @@ class SongsHandler {
       return response;
     }
   }
+
+  async putSongByIdHandler(request, h) {
+    try {
+      this._validator.validateSongPayload(request.payload);
+      const { songId } = request.params;
+
+      await this._service.editSongById(songId, request.payload);
+
+      return {
+        status: 'success',
+        message: 'Catatan berhasil diperbarui',
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
 }
 
 module.exports = SongsHandler;
