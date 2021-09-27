dd56a8efd9f5a5eeb1123b121f2311428ee3e168
Alfyando <alfyando2001@gmail.com>
2021-09-27 06:53:31 -0700

    v2 Kriteria 1 : Terdapat fitur registrasi pengguna (Menambahkan user)

---
+++ src/api/users/handler.js
@@ -0,0 +1,28 @@
+const autoBind = require('auto-bind');
+const { successResponse } = require('../../utils/responses');
+
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postUserHandler(request, h) {
+    this._validator.validateUserPayload(request.payload);
+    const { username, password, fullname } = request.payload;
+
+    const newUserId = await this._service.addUser({ username, password, fullname });
+
+    return successResponse(h, {
+      withMessage: true,
+      withData: true,
+      responseMessage: 'User berhasil ditambahkan',
+      responseData: { userId: newUserId },
+      responseCode: 201,
+    });
+  }
+}
+
+module.exports = UsersHandler;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
-const songs = require('./api/songs');
+
+// error
 const ClientError = require('./exceptions/ClientError');
+
+// songs
+const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
 
+// users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
+  const usersService = new UsersService();
   const songsService = new SongsService();
   const server = Hapi.server({
     port: process.env.PORT,
@@ -17,13 +27,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
